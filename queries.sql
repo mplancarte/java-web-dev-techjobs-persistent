d2c54b5408e248cd15352ec2c1d5c8d415daf640
miguel plancarte <plancarte2@gmail.com>
2020-07-11 09:49:01 -0500

    Connected MySQL techjobs database to the Spring App

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 ## Part 1: Test it with SQL
-
+//id is of int data type. skills is of varchar data type. employer is of varchar data type. name is of data type.
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
