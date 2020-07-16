96a1e4a6924adf1158db646ef05bd6d5a52b5995
miguel plancarte <plancarte2@gmail.com>
2020-07-16 17:02:08 -0500

    Tweaked a few errors. Finished.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -78,7 +78,7 @@ public class HomeController {
     }
     @GetMapping("view/{jobId}")
     public String displayViewJob (Model model,@PathVariable int jobId){
-        Optional optJob = jobRepository.findById(jobId);
+        Optional <Job> optJob = jobRepository.findById(jobId);
         if (optJob.isPresent()) {
             Job job = (Job) optJob.get();
             model.addAttribute("job", job);
